8bcfefef70d989b370511b3a288edb153a723311
HoTungBach <147029849+HoTungBach@users.noreply.github.com>
2023-10-12 08:14:45 +0000

    nopbai

--- s00_bailam.py
+++ s00_bailam.py
@@ -31,5 +31,8 @@ tongtienmuahang(None)                   | None
 
 #region bailam
 def tongtienmuahang(gia_truoc_thue):
-  return 'todo'
+  if gia_truoc_thue:
+    return(gia_truoc_thue*1.1)
+tongtienmuahang(gia_truoc_thue=1000000)
+  
 #endregion bailam
